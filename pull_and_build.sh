3c9032f0651c19e0d278d31485594f4c0b392621
bbcallen <bbcallen@gmail.com>
2012-03-06 14:59:47 +0800

    first commit

---
+++ pull_and_build.sh
@@ -0,0 +1,24 @@
+# fetch vlc-ports-android
+
+if [ ! -d "vlc-ports-android" ]; then
+    echo "vlc-ports/android source not found, cloning"
+    git clone git://git.videolan.org/vlc-ports/android vlc-ports-android
+else
+    echo "vlc-ports/android source found, pulling from remote master"
+    cd vlc-ports-android
+    git pull origin master
+    cd -
+fi
+
+
+
+# patch for mac build
+cd vlc-ports-android
+
+echo "Applying the patches for MacOS"
+git am ../patches/prebuild/*.patch || git am --abort
+
+
+
+# build
+sh compile.sh
