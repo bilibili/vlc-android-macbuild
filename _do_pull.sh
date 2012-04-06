0640392048d24ba2c6277f569cefe0c9be33544e
bbcallen <bbcallen@gmail.com>
2012-04-06 14:55:25 +0800

    work with known hash

---
+++ _do_pull.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+if [ -z "$BILI_VLC_TARGET" ]; then
+    echo "_do_pull.sh is not supposed been executed directly"
+    echo "execute _pull_xxx.sh instead"
+    exit 1
+fi
+
+BILI_VLC_ANDROID_HASH=26c15c2a49
+echo "pull "${BILI_VLC_TARGET}":"${BILI_VLC_ANDROID_HASH}
+
+if [ ! -d ${BILI_VLC_TARGET} ]; then
+    echo "vlc-ports/android source not found, cloning"
+    git clone git://git.videolan.org/vlc-ports/android ${BILI_VLC_TARGET}
+    cd ${BILI_VLC_TARGET}   
+    git checkout -B bili ${BILI_VLC_ANDROID_HASH}
+    cd -
+else
+    echo "vlc-ports/android source found, pulling from remote master"
+    cd ${BILI_VLC_TARGET}
+    git fetch origin
+    git checkout -B bili ${BILI_VLC_ANDROID_HASH}    
+    cd -
+fi
+
+echo "patch "${BILI_VLC_TARGET}
+cd ${BILI_VLC_TARGET}
+git am ../patches/ports-android/${BILI_VLC_TARGET}/*.patch || git am --abort
+git am ../patches/ports-android/*.patch || git am --abort
+cd -
--- _pull-cortex-a9-vfpv3-d16.sh
+++ _pull-cortex-a9-vfpv3-d16.sh
@@ -2,20 +2,4 @@
 
 export BILI_VLC_TARGET="vlc-cortex-a9-vfpv3-d16"
 
-echo "pull "${BILI_VLC_TARGET}
-
-if [ ! -d ${BILI_VLC_TARGET} ]; then
-    echo "vlc-ports/android source not found, cloning"
-    git clone git://git.videolan.org/vlc-ports/android ${BILI_VLC_TARGET}
-else
-    echo "vlc-ports/android source found, pulling from remote master"
-    cd ${BILI_VLC_TARGET}
-    git pull origin master
-    cd -
-fi
-
-echo "patch "${BILI_VLC_TARGET}
-cd ${BILI_VLC_TARGET}
-git am ../patches/ports-android/${BILI_VLC_TARGET}/*.patch || git am --abort
-git am ../patches/ports-android/*.patch || git am --abort
-cd -
+sh _do_pull.sh
