e537911fc3bde26e044905005278490ea1154e33
bbcallen <bbcallen@gmail.com>
2012-06-24 15:10:01 +0800

    pull app specific patch by name

--- _pull-app.sh
+++ _pull-app.sh
@@ -11,16 +11,16 @@ ln -s ../tarballs
 
 mkdir -p patches/ports-android/${BILI_VLC_TARGET}
 rm patches/ports-android/*.patch
-cp ../patches/ports-android/0001-*.patch patches/ports-android/
-cp ../patches/ports-android/0003-*.patch patches/ports-android/
+cp ../patches/ports-android/*-modify-compile.sh-for-MacOS-build.patch patches/ports-android/
+cp ../patches/ports-android/*-vlc-repo-reset-script.patch patches/ports-android/
 
 mkdir -p patches/vlc-on-macosx
 rm patches/vlc-on-macosx/*.patch
-cp ../patches/vlc-on-macosx/0002-*.patch patches/vlc-on-macosx/
+cp ../patches/vlc-on-macosx/*-fix-toolchain-config.patch patches/vlc-on-macosx/
 
 mkdir -p patches/vlc
 rm patches/vlc/*.patch
-cp ../patches/vlc/0002-*.patch patches/vlc/
+cp ../patches/vlc/*-zlib-fix-zlib-build-on-android.patch patches/vlc/
 
 sh ../_do_pull.sh
 
