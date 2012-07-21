0a90bae7b8d641ea0d581e466d5460f837e21c4a
bbcallen <bbcallen@gmail.com>
2012-07-21 15:07:48 +0800

    merge from vlc repo   vlc-ports/android: d4607fef33   vlc: 57cace3

--- _do_reset_vlc.sh
+++ _do_reset_vlc.sh
@@ -14,7 +14,7 @@ if [ ! -d "vlc-android" ]; then
 fi
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=e75d2024
+TESTED_HASH=57cace3
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
