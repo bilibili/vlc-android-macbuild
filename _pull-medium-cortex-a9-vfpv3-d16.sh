e09636f352d7c78adadb9c2555c309bc15505855
bbcallen <bbcallen@gmail.com>
2012-07-23 15:57:41 +0800

    build for vlc-medium-cortex-a9-vfpv3-d16

---
+++ _pull-medium-cortex-a9-vfpv3-d16.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+export BILI_VLC_ROOT="vlc-medium-cortex-a9-vfpv3-d16"
+export BILI_VLC_MODULE_MODE="module-medium"
+export BILI_VLC_TARGET_MODE="target-cortex-a9-vfpv3-d16"
+
+sh _do_pull.sh
