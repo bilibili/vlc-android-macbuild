55318a51ddba43f699ee104b24513da73e9a0ad5
bbcallen <bbcallen@gmail.com>
2012-09-17 18:25:36 +0800

    merge from vlc repo

--- _do_pull.sh
+++ _do_pull.sh
@@ -6,7 +6,7 @@ if [ -z "$BILI_VLC_ROOT" ]; then
     exit 1
 fi
 
-BILI_VLC_ANDROID_HASH=8b283d699a
+BILI_VLC_ANDROID_HASH=41d70208
 echo "pull "${BILI_VLC_ROOT}":"${BILI_VLC_ANDROID_HASH}
 
 if [ ! -d ${BILI_VLC_ROOT} ]; then
