5680026923ab4ea84cae4263272bf7e6d57e8ae2
bbcallen <bbcallen@gmail.com>
2012-10-08 15:12:46 +0800

    remove unused demo

--- VlcDemo/src/tv/danmaku/util/AppBuildConfig.java
+++
@@ -1,8 +0,0 @@
-package tv.danmaku.util;
-
-// gen/BuildConfig is not reliable by now
-public final class AppBuildConfig {
-    //public final static boolean DEBUG = BuildConfig.DEBUG;
-    //public final static boolean DEBUG = false;
-	public final static boolean DEBUG = true;
-}
