142362a2a4d4c9718a673225bd09b07d1eb8e5d9
bbcallen <bbcallen@gmail.com>
2012-12-23 20:40:46 +0800

    vlc-app: fix build

--- _pull-app.sh
+++ _pull-app.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-export BILI_VLC_ROOT="vlc-app-armv7a-neon"
+export BILI_VLC_ROOT="vlc-app-armv7a"
 export BILI_VLC_MODULE_MODE="module-full"
-export BILI_VLC_TARGET_MODE="target-armv7a-neon"
+export BILI_VLC_TARGET_MODE="target-armv7a"
 
 mkdir -p vlc-app
 cd vlc-app
@@ -15,17 +15,12 @@ ln -s ../_do_reset_vlc.sh _do_reset_vlc.sh
 
 mkdir -p patches/ports-android/${BILI_VLC_TARGET_MODE}
 rm patches/ports-android/*.patch
-cp ../patches/ports-android/*-modify-compile.sh-for-MacOS-build.patch patches/ports-android/
-cp ../patches/ports-android/*-vlc-repo-reset-script.patch patches/ports-android/
+cp ../patches/ports-android-app/*.patch patches/ports-android/
 cp ../patches/ports-android/${BILI_VLC_TARGET_MODE}/*.patch patches/ports-android/${BILI_VLC_TARGET_MODE}
-
-mkdir -p patches/vlc-on-macosx
-rm patches/vlc-on-macosx/*.patch
-cp ../patches/vlc-on-macosx/*-fix-toolchain-config.patch patches/vlc-on-macosx/
 
 mkdir -p patches/vlc
 rm patches/vlc/*.patch
-cp ../patches/vlc/*-zlib-fix-zlib-build-on-android.patch patches/vlc/
+cp ../patches/vlc-app/*-zlib-fix-zlib-build-on-android.patch patches/vlc/
 
 sh ../_do_pull.sh
 
