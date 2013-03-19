766bc339b5d51e05bdd047a30b8f17dbef640864
bbcallen <bbcallen@gmail.com>
2013-03-19 16:18:43 +0800

    bump from upstream vlc-android(0.0.11)
      vlc-android: 191d4c9f3c
      vlc: e3d6ee9

--- _do_reset_vlc.sh
+++ _do_reset_vlc.sh
@@ -14,7 +14,7 @@ if [ ! -d "vlc-android" ]; then
 fi
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=7018018
+TESTED_HASH=e3d6ee9
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
