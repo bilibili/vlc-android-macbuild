0f135fa4bfdf121490c8f1814ef1efbc1ab7f1d6
bbcallen <bbcallen@gmail.com>
2014-02-24 11:15:42 +0800

    bump from upstream vlc-android(master)
      vlc-android: 1c800f86e8
      vlc: 75cc1f0e

--- _do_pull.sh
+++ _do_pull.sh
@@ -6,7 +6,7 @@ if [ -z "$BILI_VLC_ROOT" ]; then
     exit 1
 fi
 
-BILI_VLC_ANDROID_HASH=5076bdf40c
+BILI_VLC_ANDROID_HASH=1c800f86e8
 echo "pull "${BILI_VLC_ROOT}":"${BILI_VLC_ANDROID_HASH}
 
 if [ ! -d ${BILI_VLC_ROOT} ]; then
