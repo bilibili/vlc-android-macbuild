09da3367829700b8ca0e27106c8ea02902a438c5
Ming Hu <tewilove@gmail.com>
2014-05-09 11:49:12 +0800

    fix: forgot update hash in 22a973bedfc4dffb1c78bdd09aca9bafeac2d476

--- _do_reset_vlc.sh
+++ _do_reset_vlc.sh
@@ -25,7 +25,7 @@ if [ ! -d "vlc-android" ]; then
 fi
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=4ec53cf7
+TESTED_HASH=22d0f70
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
