59e71859b43d517253121a16338ef1c127149498
Ming Hu <tewilove@gmail.com>
2014-06-09 15:09:25 +0800

    vlc: 602d02a vlc-android: de1641d

--- _do_pull.sh
+++ _do_pull.sh
@@ -6,7 +6,7 @@ if [ -z "$BILI_VLC_ROOT" ]; then
     exit 1
 fi
 
-BILI_VLC_ANDROID_HASH=42a51db
+BILI_VLC_ANDROID_HASH=de1641d
 echo "pull "${BILI_VLC_ROOT}":"${BILI_VLC_ANDROID_HASH}
 
 if [ ! -d ${BILI_VLC_ROOT} ]; then
